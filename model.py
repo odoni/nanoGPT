1c73b18c2f074f14a60155a7c8a240b5c9aee894
felipe.odoni <felipe.odoni@gmail.com>
2023-03-06 16:43:51 +0000

    prepare para maquiavel

--- model.py
+++ model.py
@@ -226,9 +226,9 @@ class GPT(nn.Module):
             'gpt2-xl':      dict(n_layer=48, n_head=25, n_embd=1600), # 1558M params
         }[model_type]
         print("forcing vocab_size=50257, block_size=1024, bias=True")
-        config_args['vocab_size'] = 50257 # always 50257 for GPT model checkpoints
-        config_args['block_size'] = 1024 # always 1024 for GPT model checkpoints
-        config_args['bias'] = True # always True for GPT model checkpoints
+        # config_args['vocab_size'] = 50257 # always 50257 for GPT model checkpoints
+        # config_args['block_size'] = 1024 # always 1024 for GPT model checkpoints
+        # config_args['bias'] = True # always True for GPT model checkpoints
         # we can override the dropout rate, if desired
         if 'dropout' in override_args:
             print(f"overriding dropout rate to {override_args['dropout']}")
