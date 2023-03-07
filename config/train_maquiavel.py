481ce44c09807eb5c11bd2e040bd2215a6158754
felipe.odoni <felipe.odoni@gmail.com>
2023-03-07 11:51:58 +0000

    prepare para maquiavel

--- config/train_maquiavel.py
+++ config/train_maquiavel.py
@@ -1,7 +1,7 @@
 # train a miniature character-level shakespeare model
 # good for debugging and playing on macbooks and such
 dtype='float32'
-init_from = 'gpt2'
+init_from = 'gpt2-xl'
 out_dir = 'out-maquiavel'
 eval_interval = 20 # keep frequent because we'll overfit
 eval_iters = 200
